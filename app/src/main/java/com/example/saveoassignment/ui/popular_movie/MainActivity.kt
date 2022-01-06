f2631d6fd1e8096142646a0d3ade56356e9327bd
Sumit Rai <developer2607@gmail.com>
2022-01-06 18:42:42 +0530

    Activity Created

--- app/src/main/java/com/example/saveoassignment/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/com/example/saveoassignment/ui/popular_movie/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.saveoassignment
+package com.example.saveoassignment.ui.popular_movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.saveoassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/saveoassignment/ui/single_movie_details/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.example.saveoassignment.ui.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.saveoassignment.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
